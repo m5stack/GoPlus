efba31c93426f1573066dda56d157e73d4bab864
yalezy <zhouyangyale@gmail.com>
2019-05-14 20:22:11 +0800

    updata test

--- src/src.ino
+++ src/src.ino
@@ -1,20 +1,22 @@
 #include <Wire.h>
 #include <Servo.h>
+//#include <MsTimer2.h> 
 
 //!servo
 Servo myservo_0,myservo_1,myservo_2,myservo_3;
 const int SERVO_0 = 8;
-const int SERVO_1 = 9;
-const int SERVO_2 = 10;
-const int SERVO_3 = 11; 
+const int SERVO_1 = 17;
+const int SERVO_2 = 2;
+const int SERVO_3 = 4; 
+uint8_t  SERVO_flag[4] = {0,0,0,0};
 
 void Servo_read_angle(uint8_t number, uint8_t angle) {
     
     switch(number){
-      case 0: myservo_0.write(angle); break;
-      case 1: myservo_1.write(angle); break;
-      case 2: myservo_2.write(angle); break;
-      case 3: myservo_3.write(angle); break;
+      case 0: if(!SERVO_flag[0]) myservo_0.attach(SERVO_0);SERVO_flag[0] = 1; myservo_0.write(angle); break;
+      case 1: if(!SERVO_flag[1]) myservo_1.attach(SERVO_1);SERVO_flag[1] = 1; myservo_1.write(angle); break;
+      case 2: if(!SERVO_flag[2]) myservo_2.attach(SERVO_2);SERVO_flag[2] = 1; myservo_2.write(angle); break;
+      case 3: if(!SERVO_flag[3]) myservo_3.attach(SERVO_3);SERVO_flag[3] = 1; myservo_3.write(angle); break;
       default:break;
     }
 }
@@ -23,15 +25,15 @@ void Servo_read_angle(uint8_t number, uint8_t angle) {
 //! 10-bit ADC
 uint16_t adInValue[6]  = {0};
 const int HubPort[6] = {A0,5,A1,6,A2,7}; 
-uint8_t port_num = 0;
-
+//uint8_t port_num = 0;
+/*
 void hub_wire_value(bool ad_flag, uint8_t value){
 
   if(ad_flag){
+    pinMode(HubPort[port_num],OUTPUT);
     digitalWrite(HubPort[port_num], value);
   }else
   {
-    /* code */
     analogWrite(HubPort[port_num], value);
   }
 } 
@@ -39,56 +41,116 @@ void hub_wire_value(bool ad_flag, uint8_t value){
 void hub_read_value(bool ad_flag){
 
   if(ad_flag){
+    pinMode(HubPort[port_num],INPUT);
     adInValue[port_num] = digitalRead(HubPort[port_num]);
   }else
   {
-    /* code */
     adInValue[port_num] = analogRead(HubPort[port_num]);
   }
 
 }
+*/
+
+
 
 
 //！motor
-const int IN_0 = 17;
-const int IN_1 = 2;
-const int IN_2 = 3;
-const int IN_3 = 4; 
-void motor_wire_value(uint8_t motor_num,uint8_t trun){
+const int IN_0 = 9;
+const int IN_1 = 10;
+const int IN_2 = 11;
+const int IN_3 = 3; 
 
-  if(trun == 0){
-    if(motor_num){
+int duty0  = 0, duty1 = 0;
+uint8_t duty_trun0 = 0,duty_trun1 = 0;
+uint8_t duty_speed0 = 0,duty_speed1 = 0;
+uint8_t duty_motor_0_flag = 0,duty_motor_1_flag = 0;
+uint8_t time_flag = 0;
+
+void motor_wire_value(uint8_t motor_num, uint8_t trun, uint8_t motor_speed){
+  
+  //duty_motor_num = motor_num;
+  if(motor_speed == 0){
+    if(motor_num == 0){
+      
       digitalWrite(IN_0, 0);
       digitalWrite(IN_1, 0);
-    }else
+      duty_trun0 = trun;
+      duty_speed0 =  motor_speed;
+      //MsTimer2::stop(); 
+      //time_flag = 0;
+      duty0 = 0;
+      duty_motor_0_flag = 0;
+
+      //analogWrite(IN_0, 0);
+      //analogWrite(IN_1, 0);
+    }else if(motor_num == 2)
     {
+     // digitalWrite(IN_2, 0);
+      //digitalWrite(IN_3, 0);
+
       digitalWrite(IN_2, 0);
       digitalWrite(IN_3, 0);
+      duty_trun1 = trun;
+      duty_speed1 =  motor_speed;
+      //MsTimer2::stop(); 
+      //time_flag = 0;
+      duty1 = 0;
+      duty_motor_1_flag = 0;
+
+      // analogWrite(IN_2, 0);
+      // analogWrite(IN_3, 0);
     } 
   }
-  else if(trun == 1)
+  else if(trun == 0x00)
   {
-    /* code */
+     if(motor_num == 0){
+     // digitalWrite(IN_0, 1);
+     // digitalWrite(IN_1, 0);
+     duty_motor_0_flag = 1;
+      //analogWrite(IN_0, motor_speed);
+      //analogWrite(IN_1, 0);
+     duty_trun0 = trun;
+     duty_speed0 =  motor_speed;
+     //MsTimer2::start();
+     time_flag = 1;
 
-     if(motor_num){
-      digitalWrite(IN_0, 1);
-      digitalWrite(IN_1, 0);
-    }else
+    }else if(motor_num == 2)
     {
-      digitalWrite(IN_2, 1);
-      digitalWrite(IN_3, 0);
+      //analogWrite(IN_2, motor_speed);
+      //analogWrite(IN_3, 0);
+      duty_motor_1_flag = 1;
+      duty_trun1 = trun;
+      duty_speed1 =  motor_speed;
+      //MsTimer2::start();
+      time_flag = 1;
     } 
 
   }
-  else if(trun == 2){
+  else if(trun == 0x80){
 
-    if(motor_num){
-      digitalWrite(IN_0, 0);
-      digitalWrite(IN_1, 1);
-    }else
+    if(motor_num == 0){
+     // digitalWrite(IN_0, 0);
+      //digitalWrite(IN_1, 1);
+
+      //analogWrite(IN_0, 0);
+     /// analogWrite(IN_1, motor_speed);
+      duty_motor_0_flag = 1;
+      duty_trun0 = trun;
+      duty_speed0 =  motor_speed;
+      //MsTimer2::start();
+      time_flag = 1;
+    }else if(motor_num == 2)
     {
-      digitalWrite(IN_2, 0);
-      digitalWrite(IN_3, 1);
+      //digitalWrite(IN_2, 0);
+      //digitalWrite(IN_3, 1);
+
+      //analogWrite(IN_2, 0);
+      //analogWrite(IN_3, motor_speed);
+      duty_motor_1_flag = 1;
+      duty_trun1 = trun;
+      duty_speed1 =  motor_speed;
+     // MsTimer2::start();
+      time_flag = 1;
     } 
 
   }
@@ -99,7 +161,7 @@ uint8_t reg;
 //uint8_t number = 0;
 void receiveEvent(int howMany) {
 	uint8_t number = 0; 
-  if ((howMany != 2) && (howMany != 3)){
+  if ((howMany != 1) &&(howMany != 2) && (howMany != 3) && (howMany != 4)){
     while (Wire.available() > 0)
       Wire.read();
     return ;
@@ -119,24 +181,114 @@ void receiveEvent(int howMany) {
   if(number == 0x30){
      uint8_t motor_num = 0;
      uint8_t turn_flag = 0;
+     uint8_t motor_speed = 0;
      motor_num = reg & 0x0f;
      turn_flag = Wire.read();
-     motor_wire_value(motor_num,turn_flag);  
+     motor_speed = Wire.read();
+     motor_wire_value(motor_num,turn_flag,motor_speed);  
   } else if(number == 0x40){
      
-    port_num = reg & (0x0f);
-    uint8_t temp = Wire.read();
-    uint8_t rw_flag = temp & 0X80;
-    uint8_t ad_flag = temp & 0X01;
-
-    if (rw_flag) {
-      uint8_t value = Wire.read();
-      hub_wire_value(ad_flag, value);
-    }else{
-       hub_read_value(ad_flag); 
+    uint8_t port_num = reg & (0x0f);
+    uint8_t value = 0;
+    switch(port_num){
+      case 0:  value = Wire.read();
+               pinMode(HubPort[0],OUTPUT);
+               digitalWrite(HubPort[0], value);
+               break;
+      case 1:  value = Wire.read();
+               pinMode(HubPort[1],OUTPUT);
+               digitalWrite(HubPort[1], value);
+               break;
+      case 2:  value = Wire.read();
+               analogWrite(HubPort[0], value);
+               break;
+      case 3:  value = Wire.read();
+               analogWrite(HubPort[1], value);
+               break;
+      case 4:  pinMode(HubPort[0],INPUT_PULLUP);
+               adInValue[0] = digitalRead(HubPort[0]);
+               break;
+      case 5:  pinMode(HubPort[1],INPUT_PULLUP);
+               adInValue[1] = digitalRead(HubPort[1]);
+               break;
+      case 6:  
+              pinMode(HubPort[0],INPUT_PULLUP);
+              adInValue[0] = analogRead(HubPort[0]);
+               break;
+      case 7:   pinMode(HubPort[1],INPUT_PULLUP);
+               adInValue[1] = analogRead(HubPort[1]);
+               break;
+      default:break;
     }
 
-  }else if(number == 0x10){
+  }else if(number == 0x50){
+     uint8_t port_num = reg & (0x0f);
+     uint8_t value = 0;
+     switch(port_num){
+       case 0:  value = Wire.read();
+               pinMode(HubPort[2],OUTPUT);
+               digitalWrite(HubPort[2], value);
+               break;
+      case 1:  value = Wire.read();
+               pinMode(HubPort[3],OUTPUT);
+               digitalWrite(HubPort[3], value);
+               break;
+      case 2:  value = Wire.read();
+               analogWrite(HubPort[2], value);
+               break;
+      case 3:  value = Wire.read();
+               analogWrite(HubPort[3], value);
+               break;
+      case 4:  pinMode(HubPort[2],INPUT_PULLUP);
+               adInValue[2] = digitalRead(HubPort[2]);
+               break;
+      case 5:  pinMode(HubPort[3],INPUT_PULLUP);
+               adInValue[3] = digitalRead(HubPort[3]);
+               break;
+      case 6: pinMode(HubPort[2],INPUT_PULLUP); 
+              adInValue[2] = analogRead(HubPort[2]);
+               break;
+      case 7:  pinMode(HubPort[3],INPUT_PULLUP); 
+               adInValue[3] = analogRead(HubPort[3]);
+              break;
+      default:break;
+      }
+
+  }else if(number == 0x60){
+      uint8_t port_num = reg & (0x0f);
+      uint8_t value = 0;
+      switch(port_num){
+        case 0:  value = Wire.read();
+                 pinMode(HubPort[4],OUTPUT);
+               digitalWrite(HubPort[4], value);
+               break;
+        case 1:  value = Wire.read();
+               pinMode(HubPort[5],OUTPUT);
+               digitalWrite(HubPort[5], value);
+               break;
+        case 2:  value = Wire.read();
+               analogWrite(HubPort[4], value);
+               break;
+        case 3:  value = Wire.read();
+               analogWrite(HubPort[5], value);
+               break;
+        case 4:  pinMode(HubPort[4],INPUT_PULLUP);
+               adInValue[4] = digitalRead(HubPort[4]);
+               break;
+        case 5:  pinMode(HubPort[5],INPUT_PULLUP);
+               adInValue[5] = digitalRead(HubPort[5]);
+               break;
+        case 6:pinMode(HubPort[4],INPUT_PULLUP);   
+               adInValue[4] = analogRead(HubPort[4]);
+               break;
+        case 7:pinMode(HubPort[5],INPUT_PULLUP);  
+              adInValue[5] = analogRead(HubPort[5]);
+              break;
+        default:break;
+        }
+
+  }
+  else if(number == 0x10){
      uint8_t angle = 0;
      uint8_t num = 0;
      num = reg & 0x0f;
@@ -182,8 +334,6 @@ void receiveEvent(int howMany) {
         uint8_t turn_flag = 0;
         motor_num = reg & 0x0f;
         turn_flag = Wire.read();
-       // uint8_t speed = Wire.read();;
-       // motor_wire_value(motor_num,turn_flag);
       break;
     default:
       break;
@@ -192,12 +342,66 @@ void receiveEvent(int howMany) {
 }
 
 void requestEvent() {
+  uint8_t port_num = reg & (0x0f);
   switch(reg & 0xf0){
     case 0x40:
-      Wire.write((uint8_t)(adInValue[port_num]&0xff));
-      Wire.write((uint8_t)(adInValue[port_num]>>8)&0x03);
-    break;
+      switch(port_num){
+      case 4:
+        Wire.write((uint8_t)(adInValue[0]&0xff));
+        break;
+      case 5:
+        Wire.write((uint8_t)(adInValue[1]&0xff));
+        break;
+      case 6:
+        Wire.write((uint8_t)(adInValue[0]&0xff));
+        Wire.write((uint8_t)(adInValue[0]>>8)&0x03);
+        break;
 
+      case 7:
+        Wire.write((uint8_t)(adInValue[1]&0xff));
+        Wire.write((uint8_t)(adInValue[1]>>8)&0x03);
+        break;
+      }
+    break;
+    case 0x50:
+       switch(port_num){
+      case 4:
+        Wire.write((uint8_t)(adInValue[2]&0xff));
+        break;
+      case 5:
+        Wire.write((uint8_t)(adInValue[3]&0xff));
+       
+        break;
+      case 6:
+        Wire.write((uint8_t)(adInValue[2]&0xff));
+        Wire.write((uint8_t)(adInValue[2]>>8)&0x03);
+        break;
+
+      case 7:
+        Wire.write((uint8_t)(adInValue[3]&0xff));
+        Wire.write((uint8_t)(adInValue[3]>>8)&0x03);
+        break;
+      }
+    break;
+    case 0x60:
+       switch(port_num){
+      case 4:
+        Wire.write((uint8_t)(adInValue[4]&0xff));
+        break;
+      case 5:
+        Wire.write((uint8_t)(adInValue[5]&0xff));
+        break;
+      case 6:
+        Wire.write((uint8_t)(adInValue[4]&0xff));
+        Wire.write((uint8_t)(adInValue[4]>>8)&0x03);
+        break;
+
+      case 7:
+        Wire.write((uint8_t)(adInValue[5]&0xff));
+        Wire.write((uint8_t)(adInValue[5]>>8)&0x03);
+        break;
+      }
+    break;
     default:break;
   }
   /*
@@ -247,26 +451,199 @@ void requestEvent() {
   }
   */
 }
+
+void Timer(void){  
+  if(duty_motor_0_flag){    
+  duty0++;
+  if(duty0 >= 128)
+     duty0 = 0;
+  }
+ if(duty_motor_1_flag){  
+ duty1++;
+  if(duty1 >= 128)
+     duty1 = 0;
+ }
+
+}
 void setup() {
   // put your setup code here, to run once:
-  Wire.begin(0x2b);
+ /*
+  pinMode(SERVO_0,OUTPUT);
+  pinMode(SERVO_1,OUTPUT);
+  pinMode(SERVO_2,OUTPUT);
+  pinMode(SERVO_3,OUTPUT);
+
+  digitalWrite(SERVO_0,HIGH);
+  digitalWrite(SERVO_1,HIGH);
+  digitalWrite(SERVO_2,HIGH);
+  digitalWrite(SERVO_3,HIGH);
+
+  */
+  Wire.begin(0x5d);
   Wire.onReceive(receiveEvent);
-	Wire.onRequest(requestEvent);
+  Wire.onRequest(requestEvent);
 
 
-  myservo_0.attach(SERVO_0); 
-  myservo_1.attach(SERVO_1); 
-  myservo_2.attach(SERVO_2); 
-  myservo_3.attach(SERVO_3); 
+  //myservo_0.attach(SERVO_0); 
+  //myservo_1.attach(SERVO_1); 
+  //myservo_2.attach(SERVO_2); 
+  //myservo_3.attach(SERVO_3); 
 
 
+  //pinMode(IN_0, OUTPUT);
+  //pinMode(IN_1, OUTPUT);
+ 
+
+
+  ///MsTimer2::set(1, Timer); 
   pinMode(IN_0, OUTPUT);
   pinMode(IN_1, OUTPUT);
   pinMode(IN_2, OUTPUT);
   pinMode(IN_3, OUTPUT);
-
+  motor_wire_value(2,0x00,0);
+  motor_wire_value(0,0x00,0);
 }
+
+uint8_t pwmflag0 = 0;
+uint8_t pwmflag1 = 0;
+uint8_t pwmflag2 = 0;
+uint8_t pwmflag3 = 0;
 
 void loop() {
 
+#if 0
+if(duty_motor_0_flag){
+
+  if(duty_trun0 == 0x80){
+
+      if(duty0 < duty_speed0){
+        digitalWrite(IN_0, 1);
+        //digitalWrite(IN_1, 0);
+      } else {
+        digitalWrite(IN_0, 0);
+        //digitalWrite(IN_1, 0);
+      }
+      digitalWrite(IN_1, 0);
+  }else if(duty_trun0 == 0x00){
+     if(duty0 < duty_speed0){
+       //digitalWrite(IN_0, 0);
+       digitalWrite(IN_1, 1);
+      } else {
+       //digitalWrite(IN_0, 0);
+       digitalWrite(IN_1, 0);
+      }
+      digitalWrite(IN_0, 0);
+  }
+ // else {
+    // digitalWrite(IN_0, 0);
+    // digitalWrite(IN_1, 0);
+  //}
+}
+
+if(duty_motor_1_flag){
+  
+    if(duty_trun1 == 0x80){
+      if(duty1 < duty_speed1){
+        digitalWrite(IN_2, 1);
+      } else {
+        digitalWrite(IN_2, 0);
+      }
+      digitalWrite(IN_3, 0);
+
+  }else if(duty_trun1 == 0x00){
+     if(duty1 < duty_speed1){
+        //digitalWrite(IN_2, 0);
+        digitalWrite(IN_3, 1);
+      } else {
+        //digitalWrite(IN_2, 0);
+        digitalWrite(IN_3, 0);
+      }
+      digitalWrite(IN_2, 0);
+  }
+  //else {
+   // digitalWrite(IN_2, 0);
+   // digitalWrite(IN_3, 0);
+  //}
+}
+#else
+  if(duty_motor_0_flag){
+    switch(duty_trun0){
+      case 0x00:
+        if(duty0 < duty_speed0/2){
+          if(!pwmflag0){
+            digitalWrite(IN_1, LOW);
+            digitalWrite(IN_0, HIGH);
+            pwmflag0 = 1;
+          }
+        } else {
+          if(pwmflag0){
+            digitalWrite(IN_0, LOW);
+            digitalWrite(IN_1, LOW);
+            pwmflag0 = 0;
+          }
+        }
+        break;
+      case 0x80:
+        if(duty0 < duty_speed0/2){
+           if(!pwmflag1){
+             digitalWrite(IN_0, LOW);
+             digitalWrite(IN_1, HIGH);
+             pwmflag1 = 1;
+           }
+        } else {
+          if(pwmflag1){
+            digitalWrite(IN_0, LOW);
+            digitalWrite(IN_1, LOW);
+            pwmflag1 = 0;
+          }
+        }
+        break;
+      default:break;
+    }
+  }
+
+  if(duty_motor_1_flag){
+    switch(duty_trun1){
+      case 0x80:
+        if(duty1 < duty_speed1/2){
+          if(!pwmflag2){
+            digitalWrite(IN_3, LOW);
+            digitalWrite(IN_2, HIGH);
+            pwmflag2 = 1;
+          }
+        } else {
+          if(pwmflag2){
+            digitalWrite(IN_2, LOW);
+            digitalWrite(IN_3, LOW);
+            pwmflag2 = 0;
+          }
+        }
+        break;
+      case 0x00: 
+         if(duty1 < duty_speed1/2){
+           if(!pwmflag3){
+             digitalWrite(IN_2, LOW);
+             digitalWrite(IN_3, HIGH);
+             pwmflag3 = 1;
+           }
+        } else {
+          if(pwmflag3){
+            digitalWrite(IN_2, LOW);
+            digitalWrite(IN_3, LOW);
+            pwmflag3 = 0;
+          }
+        }  
+        break;
+      default:break;
+    }
+
+  }
+
+#endif
+
+
+ if(time_flag){
+   Timer();
+ }
+ 
 }
--- test/test.ino
+++ test/test.ino
@@ -1,7 +1,7 @@
 #include <M5Stack.h>
 #include <Wire.h>
 
-#define GOPLUS_ADDR 0x2b
+#define GOPLUS_ADDR 0x5d
 
 #define SERVO_ADDR 0x10
 #define SERVO_NUM0 0x00
@@ -33,18 +33,35 @@ void Servo_write_angle(uint8_t number, uint8_t angle) {
     Wire.endTransmission();
 }
 
-void Motor_write_speed(uint8_t number, uint8_t trun) {
+void Motor_write_speed(uint8_t number, uint8_t trun,uint8_t motor_speed) {
     Wire.beginTransmission(GOPLUS_ADDR);
     Wire.write(0x30 | number);
     Wire.write(trun);
+    Wire.write(motor_speed);
     Wire.endTransmission();
 }
 
-uint16_t hub_read_value(uint8_t port_num, uint8_t ad_flag){
+uint16_t hub1_a_read_value(uint8_t reg){
   
   Wire.beginTransmission(GOPLUS_ADDR);
-  Wire.write(0x40 | port_num);
-  Wire.write(0x00 | ad_flag);
+  Wire.write(0x40 | reg);
+  Wire.endTransmission();
+  
+  uint8_t RegValue_L,RegValue_H;
+
+  Wire.requestFrom(GOPLUS_ADDR,1);
+  while(Wire.available()){
+    RegValue_L = Wire.read();
+  }
+  
+  return RegValue_L;   
+} 
+
+
+uint16_t hub1_d_read_value(uint8_t reg){
+  
+  Wire.beginTransmission(GOPLUS_ADDR);
+  Wire.write(0x40 | reg);
   Wire.endTransmission();
   
   uint8_t RegValue_L,RegValue_H;
@@ -55,18 +72,97 @@ uint16_t hub_read_value(uint8_t port_num, uint8_t ad_flag){
     RegValue_H = Wire.read();
   }
   
-  return (RegValue_H << 8)|RegValue_L;   
+  return (RegValue_H<<8) | RegValue_L;   
+} 
+
+uint16_t hub2_d_read_value(uint8_t reg){
+  
+  Wire.beginTransmission(GOPLUS_ADDR);
+  Wire.write(0x50 | reg);
+  Wire.endTransmission();
+  
+  uint8_t RegValue_L,RegValue_H;
+
+  Wire.requestFrom(GOPLUS_ADDR,2);
+  while(Wire.available()){
+    RegValue_L = Wire.read();
+    RegValue_H = Wire.read();
+  }
+  
+  return (RegValue_H<<8) | RegValue_L;   
+} 
+
+uint16_t hub3_d_read_value(uint8_t reg){
+  
+  Wire.beginTransmission(GOPLUS_ADDR);
+  Wire.write(0x60 | reg);
+  Wire.endTransmission();
+  
+  uint8_t RegValue_L,RegValue_H;
+
+  Wire.requestFrom(GOPLUS_ADDR,2);
+  while(Wire.available()){
+    RegValue_L = Wire.read();
+    RegValue_H = Wire.read();
+  }
+  
+  return (RegValue_H<<8) | RegValue_L;   
+} 
+
+uint16_t hub2_a_read_value(uint8_t reg){
+  
+  Wire.beginTransmission(GOPLUS_ADDR);
+  Wire.write(0x50 | reg);
+  Wire.endTransmission();
+  
+  uint8_t RegValue_L,RegValue_H;
+
+  Wire.requestFrom(GOPLUS_ADDR,1);
+  while(Wire.available()){
+    RegValue_L = Wire.read();
+  }
+  
+  return RegValue_L;   
+} 
+
+uint16_t hub3_a_read_value(uint8_t reg){
+  
+  Wire.beginTransmission(GOPLUS_ADDR);
+  Wire.write(0x60 | reg);
+  Wire.endTransmission();
+  
+  uint8_t RegValue_L,RegValue_H;
+
+  Wire.requestFrom(GOPLUS_ADDR,1);
+  while(Wire.available()){
+    RegValue_L = Wire.read();
+  }
+  
+  return RegValue_L;   
 } 
 
 
-void hub_wire_value(uint8_t port_num, uint8_t ad_flag,uint16_t value){
+void hub1_wire_value(uint8_t reg,uint16_t value){
   
   Wire.beginTransmission(GOPLUS_ADDR);
-  Wire.write(0x40 | port_num);
-  Wire.write(0x80 | ad_flag);
-  Wire.write(0x00 | ad_flag);
+  Wire.write(0x40 | reg);
   Wire.write(value & 0xff);
-  Wire.write(value >> 8);
+  Wire.endTransmission(); 
+} 
+
+void hub2_wire_value(uint8_t reg,uint16_t value){
+  
+  Wire.beginTransmission(GOPLUS_ADDR);
+  Wire.write(0x50 | reg);
+  Wire.write(value & 0xff);
+  Wire.endTransmission(); 
+} 
+
+void hub3_wire_value(uint8_t reg,uint16_t value){
+  
+  Wire.beginTransmission(GOPLUS_ADDR);
+  Wire.write(0x60 | reg);
+  Wire.write(value & 0xff);
   Wire.endTransmission(); 
 } 
 
@@ -75,31 +171,81 @@ void setup() {
    M5.begin();
    Wire.begin();
 
+   pinMode(13,OUTPUT);
+   Motor_write_speed((uint8_t)2, (uint8_t)0x80, 250);
+   Motor_write_speed((uint8_t)0, (uint8_t)0x80, 250);
+   delay(5000);
+   Motor_write_speed((uint8_t)0, (uint8_t)0x80, 0);
+   Motor_write_speed((uint8_t)2, (uint8_t)0x00, 0);
+   delay(500);
+
+
+   Motor_write_speed((uint8_t)2, (uint8_t)0x00, 250);
+   Motor_write_speed((uint8_t)0, (uint8_t)0x00, 250);
+   delay(5000);
+   Motor_write_speed((uint8_t)0, (uint8_t)0x80, 0);
+   Motor_write_speed((uint8_t)2, (uint8_t)0x00, 0);
+   delay(500);
+
 }
 uint8_t a = 20;
 uint16_t adInValue[6] = {0};
+int led = 0;
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  digitalWrite(13, led);
   Servo_write_angle(0,  a); delay(100);
   Servo_write_angle(1 , a); delay(100);
   Servo_write_angle(2 , a); delay(100);
   Servo_write_angle(3 , a); delay(100);
 
-  if (a > 90) {
-    a = 20;
+  if (a > 180) {
+    a = 10;
   }
-  a = a + 20;
-  M5.Lcd.fillScreen(BLACK);
-  for(int i = 0; i < 6; i++){
-    adInValue[i] = hub_read_value((uint8_t)i, 0);
-    delay(100);
+  a = a + 170;
+  
+  //delay(1000);
+  
+    M5.Lcd.fillScreen(BLACK);
+    adInValue[0] = hub1_d_read_value((uint8_t)6);
+    delay(50);
     //M5.Lcd.Fill(0, 30 * i, 1);
-    M5.Lcd.setCursor(0, 30 * i, 1);
-    M5.Lcd.print(adInValue[i]); 
-  }
+    M5.Lcd.setCursor(0, 30 * 0, 1);
+    M5.Lcd.print(adInValue[0]); 
+
+    adInValue[1] = hub1_d_read_value((uint8_t)7);
+    delay(50);
+    //M5.Lcd.Fill(0, 30 * i, 1);
+    M5.Lcd.setCursor(0, 30 * 1, 1);
+    M5.Lcd.print(adInValue[1]); 
 
 
-   Motor_write_speed((uint8_t)0, (uint8_t)1);delay(100);
-   Motor_write_speed((uint8_t)1, (uint8_t)1);delay(100);
+    adInValue[2] = hub2_d_read_value((uint8_t)6);
+    delay(50);
+    //M5.Lcd.Fill(0, 30 * i, 1);
+    M5.Lcd.setCursor(0, 30 * 2, 1);
+    M5.Lcd.print(adInValue[2]); 
+  
+
+
+    adInValue[3] = hub2_d_read_value((uint8_t)7);
+    delay(50);
+    //M5.Lcd.Fill(0, 30 * i, 1);
+    M5.Lcd.setCursor(0, 30 * 3, 1);
+    M5.Lcd.print(adInValue[3]); 
+
+    adInValue[4] = hub3_d_read_value((uint8_t)6);
+    delay(50);
+    //M5.Lcd.Fill(0, 30 * i, 1);
+    M5.Lcd.setCursor(0, 30 * 4, 1);
+    M5.Lcd.print(adInValue[4]); 
+
+
+    adInValue[5] = hub3_d_read_value((uint8_t)7);
+    delay(50);
+    //M5.Lcd.Fill(0, 30 * i, 1);
+    M5.Lcd.setCursor(0, 30 * 5, 1);
+    M5.Lcd.print(adInValue[5]); 
+    led = !led;
+    delay(1000);   
 }
